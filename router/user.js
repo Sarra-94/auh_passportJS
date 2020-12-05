6d80cf0366aabea1a68f124c74de491ed0d4390c
Sarra-94 <gusmi.sarra@gmail.com>
2020-12-05 21:20:54 +0100

    remove env file

--- router/user.js
+++ router/user.js
@@ -4,7 +4,9 @@ const controllers = require("../controllers/user.controllers");
 const { registerRules, validatorRules } = require("../middleware/validator");
 
 // routes
+// sign up
 router.post("/register", registerRules(), validatorRules, controllers.register);
+// sign in
 router.post("/login", controllers.login);
 
 module.exports = router;
